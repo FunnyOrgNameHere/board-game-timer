d1673c17bace7d6a916a2eb78f19d02fdcf227d5
UnixTMDev <unix@unixtm.dev>
2025-02-06 20:01:42 -0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 1337,
-    allowedHosts: ["bgtimer.local", "localhost", "10.0.0.68", "10.0.0.90", "10.0.0.24", "bgtimer.cool"],
+    allowedHosts: ["localhost", "10.0.0.68", "10.0.0.90", "10.0.0.24", "unixtm.dev"],
   },
 })
